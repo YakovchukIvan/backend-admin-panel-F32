13dfe9fb391cc20513c77cbf367acdd7cd00624f
YakovchukIvan <litvinovjobs@gmail.com>
2023-09-20 20:57:16 +0300

    add index api IP

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://3.76.253.22:5173',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   })
